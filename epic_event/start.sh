61863725ccfb59e9b89e3e5da2eb3c98705bf349
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-04 18:13:57 +0200

    build (epic-event-app): add test build

--- epic_event/start.sh
+++ epic_event/start.sh
@@ -1,4 +1,15 @@
 sleep 5
 python -m manage migrate
 python -m manage init_groups
-python -m manage runserver 0.0.0.0:8000
+if [ $TEST_ARG == 1 ]; then
+    python -m manage test
+    if [ $? -ne 0 ]; then
+        echo "Test failed !"
+        exit 1
+    else
+        echo "Test successed !"
+        exit 0
+    fi
+else
+    python -m manage runserver 0.0.0.0:8000
+fi
