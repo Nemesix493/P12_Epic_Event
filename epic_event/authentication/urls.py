878b7de230f47b85e71911d8c7d4f7b7cc22dbf8
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-12 18:40:21 +0200

    feat (authentication): add user endpoints

--- epic_event/authentication/urls.py
+++ epic_event/authentication/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .views import UserViewset
+
+user_router = SimpleRouter()
+user_router.register('user', UserViewset, basename='user')
 
 app_name = 'authentication'
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', include(user_router.urls))
 ]
