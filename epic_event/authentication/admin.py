e007647705a24287653d4528bde3c94b513dc58b
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-14 16:20:49 +0200

    feat (authentication.admin): custom admin display

--- epic_event/authentication/admin.py
+++ epic_event/authentication/admin.py
@@ -1,14 +1,35 @@
-from typing import Optional
+from typing import Any, Optional
 from django.contrib import admin
 from django.http.request import HttpRequest
 
 
 from .models import SaleMember, StaffMember, SupportMember, ManageMember
 
+
 class StaffMemberAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'username', 'email']
-    readonly_fields = ['is_staff', 'is_superuser', 'last_login', 'groups', 'user_permissions', 'date_joined']
+    readonly_fields = ['is_staff', 'is_superuser', 'last_login', 'groups', 'user_permissions', 'date_joined', 'is_active']
     exclude = ['password']
+    actions = ['deactivate']
+
+    @admin.action(
+        permissions=["change"],
+        description="Set is_active fields to False"
+    )
+    def deactivate(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+
+    def get_readonly_fields(self, request, obj=None):
+        if not obj:
+            return [field for field in self.readonly_fields if field != 'password']
+        else:
+            return self.readonly_fields
+    
+    def get_exclude(self, request: HttpRequest, obj=None) -> Any:
+        if not obj:
+            return []
+        else:
+            return self.exclude
 
 @admin.register(SaleMember)
 class SaleMemberAdmin(StaffMemberAdmin):
