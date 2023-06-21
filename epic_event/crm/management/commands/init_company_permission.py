da01bf24103c8ae1b8130363fdaecc444db7c933
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-21 09:39:37 +0200

    fix (crm.command): fix init company permission

--- epic_event/crm/management/commands/init_company_permission.py
+++ epic_event/crm/management/commands/init_company_permission.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import Group, Permission
 
 def init_company_permission():
     sale_group = Group.objects.get(name='Sale Group')
-    sale_group_all_parmissions = sale_group.permissions.all()
+    sale_group_all_permissions = sale_group.permissions.all()
+    support_group = Group.objects.get(name='Support Group')
+    support_group_all_permissions = support_group.permissions.all()
     for permission_type in ['add', 'view', 'change'] :
         for model_name in ['prospect', 'client']:
             current_permission = Permission.objects.get(codename=f'{permission_type}_{model_name}')
-            if current_permission not in sale_group_all_parmissions:
+            if current_permission not in sale_group_all_permissions:
                 sale_group.permissions.add(current_permission)
+    current_permission = Permission.objects.get(codename=f'view_client')
+    if current_permission not in support_group_all_permissions:
+                support_group.permissions.add(current_permission)
 
 class Command(BaseCommand):
 
