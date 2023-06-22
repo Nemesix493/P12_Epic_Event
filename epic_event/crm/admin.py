916aa57b3f91ce5bf6acf9023bea0ac72be4f112
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-22 23:15:11 +0200

    feat (crm): add admin interface for events

--- epic_event/crm/admin.py
+++ epic_event/crm/admin.py
@@ -1,9 +1,12 @@
-from typing import Any
+from typing import Any, Optional
 from django.contrib import admin
+from django.http.request import HttpRequest
+from django.shortcuts import render
 
 # Register your models here.
-from .models import Prospect, Client, Contract
-from authentication.models import SaleMember
+from .models import Prospect, Client, Contract, Event
+from authentication.models import SaleMember, SupportMember
+from .forms import SetSupportForm
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ['company_name','first_name', 'last_name']
@@ -34,3 +37,56 @@ class ClientAdmin(CompanyAdmin):
 class ContractAdmin(admin.ModelAdmin):
     list_display = ['client_company_name', 'amount', 'status', 'payment_due']
     readonly_fields = ['date_created', 'date_updated']
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['client_company_name', 'attendees', 'event_date']
+    readonly_fields = ['date_created', 'date_updated', 'support_contact']
+    change_list_template = 'go_to_set_support.html'
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        custom_urls = [
+            path('set-support/', self.admin_site.admin_view(self.set_support), name='set_support'),
+            path('change-support/', self.admin_site.admin_view(self.change_support), name='change_support')
+        ]
+        return custom_urls + urls
+
+    def set_support(self, request):
+        return self.support_view(
+            request=request,
+            events=Event.objects.all().filter(support_contact=None),
+            supportmembers=SupportMember.objects.all(),
+            action='admin:set_support'
+        )   
+    
+    def change_support(self, request):
+        return self.support_view(
+            request=request,
+            events=Event.objects.all().exclude(support_contact=None),
+            supportmembers=SupportMember.objects.all(),
+            action='admin:change_support'
+        )
+
+    def support_view(self, request, events, supportmembers, action):
+        if request.method == 'POST':
+            form = SetSupportForm(
+                events,
+                supportmembers,
+                request.POST
+            )
+            if form.is_valid():
+                event = events.get(pk=int(form.cleaned_data['event']))
+                event.support_contact = supportmembers.get(pk=int(form.cleaned_data['support']))
+                event.save()
+                return self.response_post_save_change(request, event)
+        else:
+            form = SetSupportForm(
+                events=events,
+                supports=supportmembers
+            )
+        context = self.admin_site.each_context(request)
+        context['form'] = form
+        context['action'] = action
+        return render(request, 'set_support.html', context)
