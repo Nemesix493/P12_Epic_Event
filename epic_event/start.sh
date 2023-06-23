3925e176209ce6d393e72c8fb6a9ef6418f7b5e5
Nemesix493 <verquerre.kevin@gmail.com>
2023-06-23 02:31:22 +0200

    build (epic_even): run db commands

--- epic_event/start.sh
+++ epic_event/start.sh
@@ -3,6 +3,8 @@ python -m manage makemigrations
 python -m manage migrate
 python -m manage init_groups
 python -m manage init_company_permission
+python -m manage init_contract_permission
+python -m manage init_event_permission
 if [ $TEST_ARG == 1 ]; then
     python -m manage test
     if [ $? -ne 0 ]; then
